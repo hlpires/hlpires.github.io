99e07c2c23e25f977b02adc1bd0791f6d9c6526d
Higor Pires <higorlpires@gmail.com>
2022-06-14 18:36:25 -0300

    commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className = 'header'>
+        <div className ='button'>aaaaaaa
+
+        </div>
+        <div className = 'headerText'>
+
+            
+        </div>
+    </div>
+  )
+}
+
+export default Header
