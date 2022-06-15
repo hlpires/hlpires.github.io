254c1d3101b1775406f4f227f14523f243e3bbb9
Higor Pires <higorlpires@gmail.com>
2022-06-15 14:02:53 -0300

    commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     <div className = 'header'>
       <div className = 'set'>
          <div className = 'headerText'>
-          Higor Pires front-end devoloper
+          <h1>H</h1>igor<h1>L</h1> <h2>Pires front-end devoloper</h2>
 
             
         </div>
