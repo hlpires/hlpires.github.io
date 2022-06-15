bb9a25617ff34a9c1c120d4167d1322850b61778
Higor Pires <higorlpires@gmail.com>
2022-06-15 14:43:11 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import './App.css'
 import Header from './components/Header'
-
+import Sobre from './components/Sobre'
 
 const something = () => {
   return (
-    <div><Header></Header></div>
+    <div>
+      <Header/>
+      <Sobre/>
+    </div>
   ) 
 }
 
---
+++ src/components/Sobre.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Sobre = () => {
+  return (
+    <div className = 'sobre'>
+      <div className = 'conteudo'></div>
+      <div className = 'conteudo'></div>
+      <div className = 'conteudo'></div>
+      
+      <div className  =' habilidades'>
+
+      </div>
+      <div className = 'perfil'>
+        <img></img>
+        <h4></h4>
+
+      </div>
+
+    </div>
+  )
+}
+
+export default Sobre
