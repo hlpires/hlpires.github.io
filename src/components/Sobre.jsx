58c1bd7df6ff096d6842e75a6c4a9bf82898437d
Higor Pires <higorlpires@gmail.com>
2022-06-20 21:39:18 -0300

    commit

--- src/components/Sobre.jsx
+++ src/components/Sobre.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Sobre = () => {
   return (
     
-    <div >
+    <div  className = 'all'>
      <div className = 'sobreTitle'><h3>Sobre</h3></div>
 
      <div className = 'sobre'>
