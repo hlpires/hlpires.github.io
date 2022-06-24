55508866ce801f57523fef142b30536d7b35aa54
Higor Pires <higorlpires@gmail.com>
2022-06-24 13:37:15 -0300

    commit

--- src/components/Conhecimentos.jsx
+++ src/components/Conhecimentos.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Conhecimentos = () => {
   return (
-    <div>Conhecimentos</div>
+    <div className = 'conhecimentos'>
+      <div className ='conhecimentoBox'> <img src="" alt="" /> <h9></h9></div>
+      <div className ='conhecimentoBox'> <img src="" alt="" /> <h9></h9></div>
+      <div className ='conhecimentoBox'> <img src="" alt="" /> <h9></h9></div>
+      <div className ='conhecimentoBox'> <img src="" alt="" /> <h9></h9></div>
+      <div className ='conhecimentoBox'> <img src="" alt="" /> <h9></h9></div>
+      <div className ='conhecimentoBox'> <img src="" alt="" /> <h9></h9></div>
+      
+    </div>
   )
 }
 
