4b707e764caab0e17ec69e57f511f434ff0ab7b4
Higor Pires <higorlpires@gmail.com>
2022-06-29 06:45:32 -0300

    commit

--- src/components/Sobre.jsx
+++ src/components/Sobre.jsx
@@ -29,18 +29,19 @@ const Sobre = () => {
      <div className = 'posicionarConteudo'>
         <div className = 'conteudo'>
           <img className = 'conteudoimage' src = {reactimg}></img>
-          
+          <h4>Mussum Ipsum, cacilds vidis litro abertis</h4>
           <h5>Design Responsivo, minhas aplicações funcionam em mobile, pc e outros dispositivos</h5>
         </div>
         <div className = 'conteudo'>
           <img className = 'conteudoimage' src = {javascript}></img>
-          
+          <h4>Mussum Ipsum, cacilds vidis litro abertis</h4>
           <h5>Sempre buscando por novas tecnologias e aplicando em meus projetos.</h5>
+         
 
         </div>
         <div className = 'conteudo'>
           <img className = 'conteudoimage' src = {html}></img>
-          
+          <h4>Mussum Ipsum, cacilds vidis litro abertis</h4>
           <h5>Websites não precisam ser estaticos, amo dar vida a eles..</h5>
         </div>
       </div>
