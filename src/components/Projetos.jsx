5aa23eace42c7cde848e660197aee16deff039f9
hlpires <higorlpires@gmail.com>
2022-11-20 19:32:35 -0300

    commit projects design changes

--- src/components/Projetos.jsx
+++ src/components/Projetos.jsx
@@ -24,7 +24,7 @@ const Projetos = () => {
 
   return (
     <div>
-    <div  className = 'projetos'>
+   {/* <div  className = 'projetos'>
       
         <div className = 'position'>
         <div className = 'sobreTitle'><h6>Projetos</h6></div>
@@ -64,23 +64,56 @@ const Projetos = () => {
         <ModalEcommerce open={modalEcommerce} onClose={() => setModalEcommerce(false)}/>
         <ModalReino open={modalReino} onClose={() => setModalReino(false)}/>
         <ModalEsports open={modalEsports} onClose={() => setModalEsports(false)}/>
-    </div>
+  </div> */}
 
     <div  className = 'projetos'>
       
       <div className = 'position'>
       <div className = 'sobreTitle'><h6>Projetos</h6></div>
-         <div className = 'projetosPosition'>
+         <div className = 'projetosPosition'  onClick = {() =>{setModalEcommerce(true)}}>
          <div data-aos = 'fade-right' className = 'projetosBox' id ='projetosBox1'>
-         
-           
+         <p className = 'projetosTextContent'>Safire</p>
+         <p className = 'projetosTextContent2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+           quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+             Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p> 
          </div>
          <div data-aos = 'fade-right' className = 'projetosBox' id ='projetosBoxArt'>
-                
+         <div className = 'imageContent' ></div>
          </div>
          
-         </div>         
+         </div>
+         <div className = 'projetosPosition' onClick = {() =>{setModalEsports(true)}}>
+         <div data-aos = 'fade-right' className = 'projetosBox' id ='projetosBox1'>
+         <p className = 'projetosTextContent'>Safire</p>
+         <p className = 'projetosTextContent2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+           quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+             Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p> 
+         </div>
+         <div data-aos = 'fade-right' className = 'projetosBox' id ='projetosBoxArt'>
+         <div className = 'imageContent'></div>
+         </div>
+         
+         </div>
+   
+         <div className = 'projetosPosition' onClick = {() =>{setModalReino(true)}}>
+         <div data-aos = 'fade-right' className = 'projetosBox' id ='projetosBox1'>
+         <p className = 'projetosTextContent'>Safire</p>
+         <p className = 'projetosTextContent2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+           quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+             Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p> 
+         </div>
+         <div data-aos = 'fade-right' className = 'projetosBox' id ='projetosBoxArt'>
+         <div className = 'imageContent'></div>
+         </div>
+         </div> 
       </div>
+      
       
       <ModalEcommerce open={modalEcommerce} onClose={() => setModalEcommerce(false)}/>
       <ModalReino open={modalReino} onClose={() => setModalReino(false)}/>
