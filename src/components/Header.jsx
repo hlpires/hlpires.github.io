50615f0ee51248bb684a84bccfd9e7df248e7b3e
hlpires <higorlpires@gmail.com>
2022-12-11 19:46:00 -0300

    design changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -19,26 +19,28 @@ const Header = () => {
   
   return (
     <div className = 'header'>
+      
       <div style = {style} className = 'menu'>
         <div className = 'menuPosition'>
+          <div className = 'menuHeader'>
         <div className = 'menuText'><Link to="sobre" spy={true} smooth={true} offset={50} duration={500}>Sobre</Link></div>
         <div className = 'menuText'><Link to="projetos" spy={true} smooth={true} offset={50} duration={500}>Projetos</Link></div>
         <div className = 'menuText'><Link to="contato" spy={true} smooth={true} offset={50} duration={500}>Contato</Link></div>
+        </div>
         </div>
       </div>
       <div style = {style} className = 'set'>
       
          <div className = 'headerText'>
+         <div className='headerArt'></div>
          <h1 className= 'animate__animated animate__lightSpeedInLeft'>Higor Pires</h1>
          <h1 id='h1S' className= 'animate__animated animate__lightSpeedInLeft'>Full stack Devoloper</h1>
          <div className = 'socialMediaBox'>
-
          <a href = 'https://github.com/hlpires'><div className = 'socialMedia' id = 'socialMedia1'></div></a>
           <Link to="contato" spy={true} smooth={true} offset={500} duration={1100}> <div className = 'socialMedia' id = 'socialMedia2'> </div></Link>
          {/* <Link spy={true} smooth={true} offset={50} duration={500}><div className = 'socialMedia' id = 'socialMedia3'></div></Link> */}
-
           </div>   
-          <div className='headerArt'></div>
+         <div className = 'buttonHeader'></div>
         </div>
      
  
