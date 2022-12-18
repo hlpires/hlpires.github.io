fbf5b3f96d21a6e2db1571a915a6d19495c0d1cc
hlpires <higorlpires@gmail.com>
2022-12-17 21:23:57 -0300

    commit icons added an buttons added to projects box

--- src/components/Projetos.jsx
+++ src/components/Projetos.jsx
@@ -74,22 +74,26 @@ const Projetos = () => {
          <div className = 'projetosPosition'  onClick = {() =>{setModalEcommerce(true)}}>
          <div data-aos = 'fade-right' className = 'projetosBox' id ='projetosBox1'>
          <p className = 'projetosTextContent'>Safire</p>
-         <div style={{border:'1px solid red',width:'90%',height:'60px',marginLeft:'5%',display:'flex'}}>
+         <div style={{width:'90%',height:'60px',marginLeft:'5%',display:'flex'}}>
            <p style = {{alignSelf:'center',fontSize:'30px',fontFamily:'fantasy',color:'white'}}>Técnologia:</p>
-           <div id = 'reactIcon' style = {{width:'60px',height:'100%',border:'1px solid red',alignSelf:'center',
+           <div id = 'reactIcon' style = {{width:'60px',height:'100%',alignSelf:'center',
           marginLeft:'2%'}}></div>
-            <div id = 'mongoIcon' style = {{width:'60px',height:'100%',border:'1px solid red',alignSelf:'center',
+            <div id = 'mongoIcon' style = {{width:'60px',height:'100%',alignSelf:'center',
           marginLeft:'2%'}}></div>
-            <div id = 'nextIcon' style = {{width:'60px',height:'100%',border:'1px solid red',alignSelf:'center',
+            <div id = 'nextIcon' style = {{width:'60px',height:'100%',alignSelf:'center',
           marginLeft:'2%'}}></div>
-            <div id= 'javascriptIcon' style = {{width:'60px',height:'100%',border:'1px solid red',alignSelf:'center',
+            <div id= 'javascriptIcon' style = {{width:'60px',height:'100%',alignSelf:'center',
           marginLeft:'2%'}}></div>
          </div>
          <p className = 'projetosTextContent2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
           sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
             Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-             Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p> 
+             Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+             <div style={{width:'90%',height:'50px',marginLeft:'5%',display:'flex'}}>
+               <a href="https://github.com/hlpires/ecommercemongodb"><div className='buttonProject'><p style={{alignSelf:'center'}} >Codigo</p></div></a>
+               <a href="https://ecommercemongodb.vercel.app/" style={{marginLeft:'5%'}}><div className='buttonProject' ><p style={{alignSelf:'center'}} >Demo</p></div></a>
+             </div>
          </div>
          <div data-aos = 'fade-right' className = 'projetosBox' id ='projetosBoxArt'>
          <div className = 'imageContent' ></div>
@@ -99,15 +103,15 @@ const Projetos = () => {
          <div className = 'projetosPosition' onClick = {() =>{setModalEsports(true)}}>
          <div data-aos = 'fade-right' className = 'projetosBox' id ='projetosBox1'>
          <p className = 'projetosTextContent'>Safire</p>
-         <div style={{border:'1px solid red',width:'90%',height:'60px',marginLeft:'5%',display:'flex'}}>
+         <div style={{width:'90%',height:'60px',marginLeft:'5%',display:'flex'}}>
            <p style = {{alignSelf:'center',fontSize:'30px',fontFamily:'fantasy',color:'white'}}>Técnologia:</p>
-           <div style = {{width:'60px',height:'100%',border:'1px solid red',alignSelf:'center',
+           <div id='reactIcon' style = {{width:'60px',height:'100%',alignSelf:'center',
           marginLeft:'2%'}}></div>
-            <div style = {{width:'60px',height:'100%',border:'1px solid red',alignSelf:'center',
+            <div id='nextIcon' style = {{width:'60px',height:'100%',alignSelf:'center',
           marginLeft:'2%'}}></div>
-            <div style = {{width:'60px',height:'100%',border:'1px solid red',alignSelf:'center',
+            <div id='javascriptIcon' style = {{width:'60px',height:'100%',alignSelf:'center',
           marginLeft:'2%'}}></div>
-            <div style = {{width:'60px',height:'100%',border:'1px solid red',alignSelf:'center',
+            <div id='apiIcon' style = {{width:'60px',height:'100%',alignSelf:'center',
           marginLeft:'2%'}}></div>
          </div>
          <p className = 'projetosTextContent2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
@@ -115,7 +119,12 @@ const Projetos = () => {
            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
             Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p> 
+             <div style={{width:'90%',height:'50px',marginLeft:'5%',display:'flex'}}>
+               <a href="https://github.com/hlpires/fastcrypto"><div className='buttonProject'><p style={{alignSelf:'center'}} >Codigo</p></div></a>
+               <a href="https://fastcrypto-nu.vercel.app/" style={{marginLeft:'5%'}}><div className='buttonProject' ><p style={{alignSelf:'center'}} >Demo</p></div></a>
+             </div>
          </div>
+   
          <div data-aos = 'fade-right' className = 'projetosBox' id ='projetosBoxArt'>
          <div className = 'imageContent'></div>
          </div>
@@ -125,22 +134,22 @@ const Projetos = () => {
          <div className = 'projetosPosition' onClick = {() =>{setModalReino(true)}}>
          <div data-aos = 'fade-right' className = 'projetosBox' id ='projetosBox1'>
          <p className = 'projetosTextContent'>Safire</p>
-         <div style={{border:'1px solid red',width:'90%',height:'60px',marginLeft:'5%',display:'flex'}}>
+         <div style={{width:'90%',height:'60px',marginLeft:'5%',display:'flex'}}>
            <p style = {{alignSelf:'center',fontSize:'30px',fontFamily:'fantasy',color:'white'}}>Técnologia:</p>
-           <div style = {{width:'60px',height:'100%',border:'1px solid red',alignSelf:'center',
+           <div id = 'solidityIcon' style = {{width:'60px',height:'100%',alignSelf:'center',
           marginLeft:'2%'}}></div>
-            <div style = {{width:'60px',height:'100%',border:'1px solid red',alignSelf:'center',
+            <div id ='metamaskIcon' style = {{width:'60px',height:'100%',alignSelf:'center',
           marginLeft:'2%'}}></div>
-            <div style = {{width:'60px',height:'100%',border:'1px solid red',alignSelf:'center',
+            <div id = 'reactIcon' style = {{width:'60px',height:'100%',alignSelf:'center',
           marginLeft:'2%'}}></div>
-            <div style = {{width:'60px',height:'100%',border:'1px solid red',alignSelf:'center',
+            <div id='ethereumIcon' style = {{width:'60px',height:'100%',alignSelf:'center',
           marginLeft:'2%'}}></div>
          </div>
          <p className = 'projetosTextContent2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
           sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
             Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-             Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p> 
+             Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
          </div>
          <div data-aos = 'fade-right' className = 'projetosBox' id ='projetosBoxArt'>
          <div className = 'imageContent'></div>
