e68570bed910bbf95d77a30f12035b99b7e3af73
Higor Pires <higorlpires@gmail.com>
2023-01-02 14:24:25 -0300

    commit

--- src/components/Contato.jsx
+++ src/components/Contato.jsx
@@ -1,47 +1,47 @@
-import React, { useRef,useState,useEffect } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import emailjs from '@emailjs/browser';
 import wpp from '../img/wpp.png'
-import {Toaster,toast} from 'react-hot-toast'
+import { Toaster, toast } from 'react-hot-toast'
 import Aos from 'aos'
 import 'aos/dist/aos.css'
 
 const Contato = () => {
 
 
-  useEffect(()=> {
+  useEffect(() => {
 
-    Aos.init({ duration:1000})
+    Aos.init({ duration: 1000 })
 
-  },[])
-
-
-    const form = useRef();
-    const [email, setEmail] = useState('');
-    
-    
+  }, [])
 
 
-    const sendEmail = (e) => {
-        e.preventDefault();
-        if(email.length === 0){
-        toast("Preencha os dados corretamente")
-        
-        }else{
-        toast("Email - enviado")
-        
-        }
-    
-        emailjs.sendForm('service_n2gylsj', 'template_d05z4jm', form.current, 'MTKoAO6h6Sg8k4bQP')
-          .then((result) => {
-              console.log(result.text);
-              
-              
-          }, (error) => {
-              console.log(error.text);
-          }); e.target.reset()
+  const form = useRef();
+  const [email, setEmail] = useState('');
 
 
-      };
+
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    if (email.length === 0) {
+      toast("Preencha os dados corretamente")
+
+    } else {
+      toast("Email - enviado")
+
+    }
+
+    emailjs.sendForm('service_n2gylsj', 'template_d05z4jm', form.current, 'MTKoAO6h6Sg8k4bQP')
+      .then((result) => {
+        console.log(result.text);
+
+
+      }, (error) => {
+        console.log(error.text);
+      }); e.target.reset()
+
+
+  };
 
 
 
@@ -51,42 +51,34 @@ const Contato = () => {
   return (
 
 
-    <div className = 'contato' id = 'contato'>
-      
-        <div className ='position'>
-          <div className = 'sobreTitle'><h6>Contato</h6></div>
+    <div className='contato' id='contato'>
 
-          <div data-aos = 'fade-right' className = 'email'>
-            <form ref={form} onSubmit={sendEmail}>
-      
-             <input className='nomeInput' type="text" placeholder= ' Nome'name="user_name" />
-     
-             <input className='emailInput' onChange = {event => setEmail(event.target.value)} type="email" placeholder= ' Seu Email' name="user_email" />
-      
-             <textarea className='messageInput' name="message" placeholder= ' Menssagem' />
-             <input className='buttonInput' type="submit" value="Enviar" />
-             </form>
-           </div>
+      <div className='position'>
+        <div className='sobreTitle'><h6>Contato</h6></div>
 
-           <div data-aos = 'fade-right' className = 'telefones'>
-               <div className = 'telefoneText'>Celular:</div>
-               <div className ='celular'>11970127930</div>
-               <div className = 'telefoneText'>Email:</div>
-               <div className = 'endereço'>higorlpires2@gmail.com</div>
-               <div className ='wppButton'>
-                 <a href = 'https://api.whatsapp.com/send?phone=5511970127930'><img className = 'contatowpp'  src={wpp} alt="" /></a>
-               </div>
-               <div className = 'socialMediaBox1'>
+        <div data-aos='fade-right' className='email'>
+          <form ref={form} onSubmit={sendEmail}>
 
-              <div className = 'socialMedia1' id = 'socialMedia1'></div>
-             {/*  <div className = 'socialMedia1' id = 'socialMedia2'></div> */}
-              <div className = 'socialMedia1' id = 'socialMedia3'></div>
+            <input className='nomeInput' type="text" placeholder=' Nome' name="user_name" />
 
-              </div>   
+            <input className='emailInput' onChange={event => setEmail(event.target.value)} type="email" placeholder=' Seu Email' name="user_email" />
 
-           </div>
+            <textarea className='messageInput' name="message" placeholder=' Menssagem' />
+            <input className='buttonInput' type="submit" value="Enviar" />
+          </form>
         </div>
-        <Toaster/>
+
+        <div data-aos='fade-right' className='telefones'>
+          <div className='telefoneText'>Celular:</div>
+          <div className='celular'>11970127930</div>
+          <div className='telefoneText'>Email:</div>
+          <div className='endereço'>higorlpires2@gmail.com</div>
+          <div className='wppButton'>
+            <a href='https://api.whatsapp.com/send?phone=5511970127930'><img className='contatowpp' src={wpp} alt="" /></a>
+          </div>
+        </div>
+      </div>
+      <Toaster />
     </div>
   )
 }
--- src/components/Projetos.jsx
+++ src/components/Projetos.jsx
@@ -1,4 +1,4 @@
-import React,{useState,useEffect}from 'react'
+import React, { useState, useEffect } from 'react'
 import animal from '../img/animal.png'
 import code from '../img/code.png'
 import play from '../img/play.png'
@@ -13,117 +13,147 @@ import { width } from 'dom-helpers'
 
 const Projetos = () => {
 
-  const[modalEcommerce,setModalEcommerce] = useState(false)
-  const[modalEsports,setModalEsports] = useState(false)
-  const[modalReino,setModalReino] = useState(false)
+  const [modalEcommerce, setModalEcommerce] = useState(false)
+  const [modalEsports, setModalEsports] = useState(false)
+  const [modalReino, setModalReino] = useState(false)
 
-  useEffect(()=> {
+  useEffect(() => {
 
-    Aos.init({ duration:1000})
+    Aos.init({ duration: 1000 })
 
-  },[])
+  }, [])
 
   return (
     <div>
-   {/* onClick = {() =>{setModalEsports(true)}} 
+      {/* onClick = {() =>{setModalEsports(true)}} 
    () =>{setModalEcommerce(true)}
    () =>{setModalReino(true)} */}
-  
-    <div  className = 'projetos'>
-      
-      <div className = 'position'>
-      <div className = 'sobreTitle'><h6>Projetos</h6></div>
-         <div className = 'projetosPosition'  >
-         <div data-aos = 'fade-right' className = 'projetosBox' id ='projetosBox1'>
-         <p className = 'projetosTextContent'>Safire</p>
-         <div style={{width:'90%',height:'60px',marginLeft:'5%',display:'flex'}}>
-           <p style = {{alignSelf:'center',fontSize:'30px',fontFamily:'fantasy',color:'white'}}>Técnologia:</p>
-           <div id = 'reactIcon' style = {{width:'60px',height:'100%',alignSelf:'center',
-          marginLeft:'2%'}}></div>
-            <div id = 'mongoIcon' style = {{width:'60px',height:'100%',alignSelf:'center',
-          marginLeft:'2%'}}></div>
-            <div id = 'nextIcon' style = {{width:'60px',height:'100%',alignSelf:'center',
-          marginLeft:'2%'}}></div>
-            <div id= 'javascriptIcon' style = {{width:'60px',height:'100%',alignSelf:'center',
-          marginLeft:'2%'}}></div>
-         </div>
-         <p className = 'projetosTextContent2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-           quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-             Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-             <div style={{width:'90%',height:'50px',marginLeft:'5%',display:'flex'}}>
-               <a href="https://github.com/hlpires/ecommercemongodb"><div className='buttonProject'><p style={{alignSelf:'center'}} >Codigo</p></div></a>
-               <a href="https://ecommercemongodb.vercel.app/" style={{marginLeft:'5%'}}><div className='buttonProject' ><p style={{alignSelf:'center'}} >Demo</p></div></a>
-             </div>
-         </div>
-         <div data-aos = 'fade-right' className = 'projetosBox' id ='projetosBoxArt'>
-         <div id = 'imageContent' ></div>
-         </div>
-         
-         </div>
-         <div className = 'projetosPosition' >
-         <div data-aos = 'fade-right' className = 'projetosBox' id ='projetosBox1'>
-         <p className = 'projetosTextContent'>Safire</p>
-         <div style={{width:'90%',height:'60px',marginLeft:'5%',display:'flex'}}>
-           <p style = {{alignSelf:'center',fontSize:'30px',fontFamily:'fantasy',color:'white'}}>Técnologia:</p>
-           <div id='reactIcon' style = {{width:'60px',height:'100%',alignSelf:'center',
-          marginLeft:'2%'}}></div>
-            <div id='nextIcon' style = {{width:'60px',height:'100%',alignSelf:'center',
-          marginLeft:'2%'}}></div>
-            <div id='javascriptIcon' style = {{width:'60px',height:'100%',alignSelf:'center',
-          marginLeft:'2%'}}></div>
-            <div id='apiIcon' style = {{width:'60px',height:'100%',alignSelf:'center',
-          marginLeft:'2%'}}></div>
-         </div>
-         <p className = 'projetosTextContent2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-           quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-             Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p> 
-             <div style={{width:'90%',height:'50px',marginLeft:'5%',display:'flex'}}>
-               <a href="https://github.com/hlpires/fastcrypto"><div className='buttonProject'><p style={{alignSelf:'center'}} >Codigo</p></div></a>
-               <a href="https://fastcrypto-nu.vercel.app/" style={{marginLeft:'5%'}}><div className='buttonProject' ><p style={{alignSelf:'center'}} >Demo</p></div></a>
-             </div>
-         </div>
-   
-         <div data-aos = 'fade-right' className = 'projetosBox' id ='projetosBoxArt'>
-         <div id = 'imageContent2'></div>
-         </div>
-         
-         </div>
-   
-         <div className = 'projetosPosition' >
-         <div data-aos = 'fade-right' className = 'projetosBox' id ='projetosBox1'>
-         <p className = 'projetosTextContent'>Safire</p>
-         <div style={{width:'90%',height:'60px',marginLeft:'5%',display:'flex'}}>
-           <p style = {{alignSelf:'center',fontSize:'30px',fontFamily:'fantasy',color:'white'}}>Técnologia:</p>
-           <div id = 'solidityIcon' style = {{width:'60px',height:'100%',alignSelf:'center',
-          marginLeft:'2%'}}></div>
-            <div id ='metamaskIcon' style = {{width:'60px',height:'100%',alignSelf:'center',
-          marginLeft:'2%'}}></div>
-            <div id = 'reactIcon' style = {{width:'60px',height:'100%',alignSelf:'center',
-          marginLeft:'2%'}}></div>
-            <div id='ethereumIcon' style = {{width:'60px',height:'100%',alignSelf:'center',
-          marginLeft:'2%'}}></div>
-         </div>
-         <p className = 'projetosTextContent2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-           quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-             Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-         </div>
-         <div data-aos = 'fade-right' className = 'projetosBox' id ='projetosBoxArt'>
-         <div className = 'imageContent'></div>
-         </div>
-         </div> 
+
+      <div className='projetos'>
+
+        <div className='position'>
+          <div className='sobreTitle'><h6>Projetos</h6></div>
+          <div className='projetosPosition'  >
+            <div data-aos='fade-right' className='projetosBox' id='projetosBox1'>
+              <p className='projetosTextContent'>Safire</p>
+              <div style={{ width: '90%', height: '50px', marginLeft: '5%', display: 'flex' }}>
+                <p style={{ alignSelf: 'center', fontSize: '26px', fontFamily: 'fantasy', color: 'white' }}>Técnologia:</p>
+                <div id='reactIcon' style={{
+                  width: '50px', height: '100%', alignSelf: 'center',
+                  marginLeft: '2%'
+                }}></div>
+                <div id='mongoIcon' style={{
+                  width: '50px', height: '100%', alignSelf: 'center',
+                  marginLeft: '2%'
+                }}></div>
+                <div id='nextIcon' style={{
+                  width: '50px', height: '100%', alignSelf: 'center',
+                  marginLeft: '2%'
+                }}></div>
+                <div id='javascriptIcon' style={{
+                  width: '50px', height: '100%', alignSelf: 'center',
+                  marginLeft: '2%'
+                }}></div>
+              </div>
+              <div style={{ maxHeight: '230px', overflowY: 'hidden' }}>
+                <p className='projetosTextContent2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                  sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                  Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                  Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              </div>
+              <div style={{ width: '90%', height: '50px', marginLeft: '5%', display: 'flex', marginTop: '20px' }}>
+                <a href="https://github.com/hlpires/ecommercemongodb"><div className='buttonProject'><p style={{ alignSelf: 'center' }} >Codigo</p></div></a>
+                <a href="https://ecommercemongodb.vercel.app/" style={{ marginLeft: '5%' }}><div className='buttonProject' ><p style={{ alignSelf: 'center' }} >Demo</p></div></a>
+              </div>
+            </div>
+            <div data-aos='fade-right' className='projetosBox' id='projetosBoxArt'>
+              <div id='imageContent' ></div>
+            </div>
+
+          </div>
+          <div className='projetosPosition' >
+            <div data-aos='fade-right' className='projetosBox' id='projetosBox1'>
+              <p className='projetosTextContent'>Safire</p>
+              <div style={{ width: '90%', height: '50px', marginLeft: '5%', display: 'flex' }}>
+                <p style={{ alignSelf: 'center', fontSize: '26px', fontFamily: 'fantasy', color: 'white' }}>Técnologia:</p>
+                <div id='reactIcon' style={{
+                  width: '50px', height: '100%', alignSelf: 'center',
+                  marginLeft: '2%'
+                }}></div>
+                <div id='nextIcon' style={{
+                  width: '50px', height: '100%', alignSelf: 'center',
+                  marginLeft: '2%'
+                }}></div>
+                <div id='javascriptIcon' style={{
+                  width: '50px', height: '100%', alignSelf: 'center',
+                  marginLeft: '2%'
+                }}></div>
+                <div id='apiIcon' style={{
+                  width: '50px', height: '100%', alignSelf: 'center',
+                  marginLeft: '2%'
+                }}></div>
+              </div>
+              <div style={{ maxHeight: '230px', overflowY: 'hidden' }}>
+                <p className='projetosTextContent2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                  sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                  Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                  Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              </div>
+              <div style={{ width: '90%', height: '50px', marginLeft: '5%', display: 'flex', marginTop: '2%' }}>
+                <a href="https://github.com/hlpires/fastcrypto"><div className='buttonProject'><p style={{ alignSelf: 'center' }} >Codigo</p></div></a>
+                <a href="https://fastcrypto-nu.vercel.app/" style={{ marginLeft: '5%' }}><div className='buttonProject' ><p style={{ alignSelf: 'center' }} >Demo</p></div></a>
+              </div>
+            </div>
+
+            <div data-aos='fade-right' className='projetosBox' id='projetosBoxArt'>
+              <div id='imageContent2'></div>
+            </div>
+
+          </div>
+
+          <div className='projetosPosition' >
+            <div data-aos='fade-right' className='projetosBox' id='projetosBox1'>
+              <p className='projetosTextContent'>Safire</p>
+              <div style={{ width: '90%', height: '50px', marginLeft: '5%', display: 'flex' }}>
+                <p style={{ alignSelf: 'center', fontSize: '26px', fontFamily: 'fantasy', color: 'white' }}>Técnologia:</p>
+                <div id='solidityIcon' style={{
+                  width: '50px', height: '100%', alignSelf: 'center',
+                  marginLeft: '2%'
+                }}></div>
+                <div id='metamaskIcon' style={{
+                  width: '50px', height: '100%', alignSelf: 'center',
+                  marginLeft: '2%'
+                }}></div>
+                <div id='reactIcon' style={{
+                  width: '50px', height: '100%', alignSelf: 'center',
+                  marginLeft: '2%'
+                }}></div>
+                <div id='ethereumIcon' style={{
+                  width: '50px', height: '100%', alignSelf: 'center',
+                  marginLeft: '2%'
+                }}></div>
+              </div>
+              <div style={{ maxHeight: '230px', overflowY: 'hidden' }}>
+                <p className='projetosTextContent2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                  sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                  Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                  Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              </div>
+            </div>
+            <div data-aos='fade-right' className='projetosBox' id='projetosBoxArt'>
+              <div className='imageContent'></div>
+            </div>
+          </div>
+        </div>
+
+
+        <ModalEcommerce open={modalEcommerce} onClose={() => setModalEcommerce(false)} />
+        <ModalReino open={modalReino} onClose={() => setModalReino(false)} />
+        <ModalEsports open={modalEsports} onClose={() => setModalEsports(false)} />
       </div>
-      
-      
-      <ModalEcommerce open={modalEcommerce} onClose={() => setModalEcommerce(false)}/>
-      <ModalReino open={modalReino} onClose={() => setModalReino(false)}/>
-      <ModalEsports open={modalEsports} onClose={() => setModalEsports(false)}/>
-  </div>
     </div>
   )
 }
