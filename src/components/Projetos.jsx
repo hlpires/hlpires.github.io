ef879f47919a9e57e13d005eddf4402ab9231149
Higor Pires <higorlpires@gmail.com>
2023-01-09 13:59:31 -0300

    commit

--- src/components/Projetos.jsx
+++ src/components/Projetos.jsx
@@ -16,46 +16,50 @@ const Projetos = () => {
   const [modalEcommerce, setModalEcommerce] = useState(false)
   const [modalEsports, setModalEsports] = useState(false)
   const [modalReino, setModalReino] = useState(false)
+  const [more, setMore] = useState(3)
+  const [show, setShow] = useState('')
+
+
   const projetos = [
     {
-      nome:'Safire',
+      nome: 'Safire',
       image: 'imageContent',
-      tecnologia:['reactIcon','mongoIcon','nextIcon','javascriptIcon'],
-      texto:'Comercio online completo, com sistema de carrinho de compras, pagamentos, login, banco de dados proprio e area de registro de novos produtos.',
-      codigo:'https://github.com/hlpires/ecommercemongodb',
-      demo:'https://ecommercemongodb.vercel.app/'
+      tecnologia: ['reactIcon', 'mongoIcon', 'nextIcon', 'javascriptIcon'],
+      texto: 'Comercio online completo, com sistema de carrinho de compras, pagamentos, login, banco de dados proprio e area de registro de novos produtos.',
+      codigo: 'https://github.com/hlpires/ecommercemongodb',
+      demo: 'https://ecommercemongodb.vercel.app/'
     },
     {
-      nome:'Fast Crypto',
+      nome: 'Fast Crypto',
       image: 'imageContent2',
-      tecnologia:['reactIcon','mongoIcon','apiIcon','javascriptIcon'],
-      texto:'Aplicação web para consulta de dados de crypto moedas, para analises de mercado utilizando gráficos',
-      codigo:'https://github.com/hlpires/fastcrypto',
-      demo:'https://fastcrypto-nu.vercel.app/'
+      tecnologia: ['reactIcon', 'mongoIcon', 'apiIcon', 'javascriptIcon'],
+      texto: 'Aplicação web para consulta de dados de crypto moedas, para analises de mercado utilizando gráficos',
+      codigo: 'https://github.com/hlpires/fastcrypto',
+      demo: 'https://fastcrypto-nu.vercel.app/'
     },
     {
-      nome:'Web 3 project',
+      nome: 'Web 3 project',
       image: 'imageContent3',
-      tecnologia:['solidityIcon','metamaskIcon','reactIcon','ethereumIcon'],
-      texto:'Mercado de compra e vendas de NFTS, utlizando a rede Ethereum.',
-      codigo:'',
-      demo:''
+      tecnologia: ['solidityIcon', 'metamaskIcon', 'reactIcon', 'ethereumIcon'],
+      texto: 'Mercado de compra e vendas de NFTS, utlizando a rede Ethereum.',
+      codigo: '',
+      demo: ''
     },
     {
-      nome:'Tecno Center',
+      nome: 'Tecno Center',
       image: 'imageContent4',
-      tecnologia:['reactIcon','javascriptIcon','apiIcon',''],
-      texto:'Comercio online que utiliza uma api para fazer o banco de dados',
-      codigo:'https://github.com/hlpires/ecommercemongodb',
-      demo:'https://ecommerceapp-self.vercel.app/'
+      tecnologia: ['reactIcon', 'javascriptIcon', 'apiIcon', ''],
+      texto: 'Comercio online que utiliza uma api para fazer o banco de dados',
+      codigo: 'https://github.com/hlpires/ecommercemongodb',
+      demo: 'https://ecommerceapp-self.vercel.app/'
     },
     {
-      nome:'Reino Animal',
+      nome: 'Reino Animal',
       image: 'imageContent5',
-      tecnologia:['javascriptIcon','','',''],
-      texto:'Aplicação Web, para promoção e divulgação de serviços de uma Petshop de Caruaru SP',
-      codigo:'https://github.com/hlpires/reinoAnimalProject',
-      demo:'https://hlpires.github.io/reinoAnimalProject/'
+      tecnologia: ['javascriptIcon', '', '', ''],
+      texto: 'Aplicação Web, para promoção e divulgação de serviços de uma Petshop de Caruaru SP',
+      codigo: 'https://github.com/hlpires/reinoAnimalProject',
+      demo: 'https://hlpires.github.io/reinoAnimalProject/'
     },
   ]
 
@@ -75,48 +79,52 @@ const Projetos = () => {
 
         <div className='position'>
           <div className='sobreTitle'><h6>Projetos</h6></div>
-          {projetos.map(({nome,tecnologia,texto,codigo,demo,image}) =>{
-            
-            return(
-            <div><div className='projetosPosition'>
-            <div data-aos='fade-right' className='projetosBox' id='projetosBox1'>
-              <p className='projetosTextContent'>{nome}</p>
-              <div style={{ width: '90%', height: '50px', marginLeft: '5%', display: 'flex' }}>
-                <p style={{ alignSelf: 'center', fontSize: '26px', fontFamily: 'fantasy', color: 'white' }}>Técnologia:</p>
-                <div id={tecnologia[0]} style={{
-                  width: '50px', height: '100%', alignSelf: 'center',
-                  marginLeft: '2%'
-                }}></div>
-                <div id={tecnologia[1]} style={{
-                  width: '50px', height: '100%', alignSelf: 'center',
-                  marginLeft: '2%'
-                }}></div>
-                <div id={tecnologia[2]}  style={{
-                  width: '50px', height: '100%', alignSelf: 'center',
-                  marginLeft: '2%'
-                }}></div>
-                <div id={tecnologia[3]}  style={{
-                  width: '50px', height: '100%', alignSelf: 'center',
-                  marginLeft: '2%'
-                }}></div>
+          {projetos.slice(0, more).map(({ nome, tecnologia, texto, codigo, demo, image }) => {
+            return (
+              <div><div className='projetosPosition'>
+                <div data-aos='fade-right' className='projetosBox' id='projetosBox1'>
+                  <p className='projetosTextContent'>{nome}</p>
+                  <div style={{ width: '90%', height: '50px', marginLeft: '5%', display: 'flex' }}>
+                    <p style={{ alignSelf: 'center', fontSize: '26px', fontFamily: 'fantasy', color: 'white' }}>Técnologia:</p>
+                    <div id={tecnologia[0]} style={{
+                      width: '50px', height: '100%', alignSelf: 'center',
+                      marginLeft: '2%'
+                    }}></div>
+                    <div id={tecnologia[1]} style={{
+                      width: '50px', height: '100%', alignSelf: 'center',
+                      marginLeft: '2%'
+                    }}></div>
+                    <div id={tecnologia[2]} style={{
+                      width: '50px', height: '100%', alignSelf: 'center',
+                      marginLeft: '2%'
+                    }}></div>
+                    <div id={tecnologia[3]} style={{
+                      width: '50px', height: '100%', alignSelf: 'center',
+                      marginLeft: '2%'
+                    }}></div>
+                  </div>
+                  <div style={{ maxHeight: '230px', overflowY: 'hidden' }}>
+                    <p className='projetosTextContent2'>{texto}</p>
+                  </div>
+                  {codigo && <div style={{ width: '90%', height: '50px', marginLeft: '5%', display: 'flex', marginTop: '20px', marginBottom: '20px' }}>
+                    <a href={codigo}><div className='buttonProject'><p style={{ alignSelf: 'center' }} >Codigo</p></div></a>
+                    <a href={demo} style={{ marginLeft: '5%' }}><div className='buttonProject' ><p style={{ alignSelf: 'center' }} >Demo</p></div></a>
+                  </div>}
+                </div>
+                <div data-aos='fade-right' className='projetosBox' id='projetosBoxArt' style={{ minHeight: '350px' }}>
+                  <div id={image} style={{ minHeight: '350px' }} ></div>
+                </div>
               </div>
-              <div style={{ maxHeight: '230px', overflowY: 'hidden' }}>
-                <p className='projetosTextContent2'>{texto}</p>
               </div>
-              {codigo && <div style={{ width: '90%', height: '50px', marginLeft: '5%', display: 'flex', marginTop: '20px', marginBottom: '20px' }}>
-                <a href={codigo}><div className='buttonProject'><p style={{ alignSelf: 'center' }} >Codigo</p></div></a>
-                <a href={demo} style={{ marginLeft: '5%' }}><div className='buttonProject' ><p style={{ alignSelf: 'center' }} >Demo</p></div></a>
-              </div> }
-            </div>
-            <div data-aos='fade-right' className='projetosBox' id='projetosBoxArt' style={{ minHeight: '350px' }}>
-              <div id={image}style={{ minHeight: '350px' }} ></div>
-            </div>
-          </div>
-          </div>
-          )
-          })} 
-          <div style = {{height:'60px', border:'1px solid red', width:'30%', marginTop:'50px', marginBottom:'50px',marginLeft:'35%'}}>
-
+            )
+          })}
+          <div style={{ display: show }} onClick={
+            () => {
+              setMore(5)
+              setShow('none')
+            }
+          } className='buttonExibir'>
+            <p style={{ alignSelf: 'center', }}>Exibir mais</p>
           </div>
         </div>
         <ModalEcommerce open={modalEcommerce} onClose={() => setModalEcommerce(false)} />
--- src/components/Sobre.jsx
+++ src/components/Sobre.jsx
@@ -1,4 +1,4 @@
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import backend from '../img/backend.png'
 import frontend from '../img/frontend.png'
 import ferramentas from '../img/ferramentas.png'
@@ -9,68 +9,68 @@ import 'aos/dist/aos.css'
 
 const Sobre = () => {
 
-  useEffect(()=> {
+  useEffect(() => {
 
-    Aos.init({ duration:1000})
+    Aos.init({ duration: 1000 })
 
-  },[])
-  
-  const [openModal,setOpenModal] = useState(false)
+  }, [])
+
+  const [openModal, setOpenModal] = useState(false)
 
   return (
-    
-  <div className= 'sobreall' id='sobre' >
-   <div className = 'position'>
-     <div data-aos = 'fade-right' className = 'sobreTitle'><h6>Sobre</h6></div>
-     <div className = 'sobre'>
-       <div  className = 'sobreText'>
-         <img src={art} data-aos = 'fade-right' className = 'art' alt="" />
 
-         <div data-aos = 'fade-right' className = 'text'>
-           
-         <h3>Olá eu sou Higor</h3>
-         <h2>Sou um Full Stack Devoloper, atuo no desenvolvimento web desde 2019. Sempre estou aprendendo tecnologias novas e aplicando em meus projetos </h2>
-            
-            
+    <div className='sobreall' id='sobre' >
+      <div className='position'>
+        <div data-aos='fade-right' className='sobreTitle'><h6>Sobre</h6></div>
+        <div className='sobre'>
+          <div className='sobreText'>
+            <img src={art} data-aos='fade-right' className='art' alt="" />
+
+            <div data-aos='fade-right' className='text'>
+
+              <h3>Olá eu sou Higor</h3>
+              <h2>Sou um Full Stack Devoloper, atuo no desenvolvimento web desde 2019. Sempre estou aprendendo tecnologias novas e aplicando em meus projetos </h2>
+
+
             </div>
-            <div  className = 'curriculo' onClick = {() => setOpenModal(true)}>Curriculo</div>
-            <Modal open={openModal} onClose={() => setOpenModal(false)}/>
-       </div>
-       
-     
+            <div className='curriculo' onClick={() => setOpenModal(true)}><p style={{ alignSelf: 'center', marginBottom: '5px' }}>Curriculo</p></div>
+            <Modal open={openModal} onClose={() => setOpenModal(false)} />
+          </div>
 
 
 
 
 
-     <div className = 'posicionarConteudo'>
-        <div data-aos = 'fade-right' className = 'conteudo' id = 'conteudo1'>
-          <img className = 'conteudoimage'  src = {frontend}></img>
-          <h4>Front-End</h4>
-          <h5>HTML, CSS, Javascript, React, Next.JS </h5>
+
+
+          <div className='posicionarConteudo'>
+            <div data-aos='fade-right' className='conteudo' id='conteudo1'>
+              <img className='conteudoimage' src={frontend}></img>
+              <h4>Front-End</h4>
+              <h5>HTML, CSS, Javascript, React, Next.JS </h5>
+            </div>
+            <div data-aos='fade-right' className='conteudo' id='conteudo2'>
+              <img className='conteudoimage' src={backend}></img>
+              <h4>Back-End</h4>
+              <h5>Node Js, MongoDB</h5>
+
+
+            </div>
+            <div data-aos='fade-right' className='conteudo' id='conteudo3'>
+              <img className='conteudoimage' src={ferramentas}></img>
+              <h4>Ferramentas</h4>
+              <h5>Figma, Photoshop, Git, Github</h5>
+            </div>
+
+          </div>
+
+
         </div>
-        <div data-aos = 'fade-right' className = 'conteudo' id = 'conteudo2'>
-          <img className = 'conteudoimage'  src = {backend}></img>
-          <h4>Back-End</h4>
-          <h5>Node Js, MongoDB</h5>
-         
 
-        </div>
-        <div data-aos = 'fade-right' className = 'conteudo' id = 'conteudo3'>
-          <img className = 'conteudoimage' src = {ferramentas}></img>
-          <h4>Ferramentas</h4>
-          <h5>Figma, Photoshop, Git, Github</h5>
-        </div>
-      
+
+
       </div>
-   
-    
-     </div>
-    
- 
-
-     </div>
-  </div>
+    </div>
   )
 }
 
