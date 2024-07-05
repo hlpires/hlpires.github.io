020ad17bc77961ae937bfb5cf4d771ce8afb9e02
hlpires <higorlpires@gmail.com>
2024-07-05 15:21:08 -0300

    upd

--- src/App.js
+++ src/App.js
@@ -2,21 +2,26 @@ import React, { useState, useEffect } from 'react'
 import './App.css'
 // import Modal from './components/Modal'
 import Index from './components/Index'
+import Header from './components/Header'
+import Modal from './components/Modal'
+import Sobre from './components/Sobre'
+import Projetos from './components/Projetos'
+import Contato from './components/Contato'
 import './App.css'
 
 
 
-function App () {
+function App() {
   //main
   return (
     <div>
-      <Header/>
+      <Header />
       <Sobre />
       <Modal />
-      <Conhecimentos/>
-      <Projetos/>
-      <Contato/>
-    </div> )
+      {/* <Conhecimentos/> */}
+      <Projetos />
+      <Contato />
+    </div>)
 }
 
 export default App
