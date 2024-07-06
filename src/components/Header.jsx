8d67e3d76ea9ed8c073b166c60890193ac2b6661
hlpires <higorlpires@gmail.com>
2024-07-06 17:51:10 -0300

    header fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -240,9 +240,9 @@ const Header = (props) => {
         <div className='set' >
           <div className='headerText'>
             <div className='headerArt'></div>
-            <div>
-              <h1 style={{ paddingTop: "20px", fontSize: "2.5vw" }}>Higor Pires</h1>
-              <h1 id='h1S' style={{ fontSize: "20px", fontSize: "1vw" }} >Full stack Developer</h1>
+            <div style={{ width: "300px", marginLeft: "45px" }}>
+              <h1 style={{ paddingTop: "20px", fontSize: "45px", whiteSpace: "nowrap" }}>Higor Pires</h1>
+              <h1 id='h1S' style={{ fontSize: "20px", fontSize: "20px" }} >Full stack Developer</h1>
               <div className='socialMediaBox'>
                 <a href='https://github.com/hlpires'><div className='socialMedia' id='socialMedia1'></div></a>
                 <a href='https://www.linkedin.com/in/higor-piresdev/'><div className='socialMedia' id='socialMedia3'></div></a>
